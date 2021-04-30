4db9a99e809c5474a8542f0b6092370b3496e521
Guido-95 <guidocarna1995@gmail.com>
2021-04-30 14:40:14 +0200

    Esercizio ordinameto stringhe

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// 1. chiedi all'utente il cognome
+// 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+// array cognomi iniziale
+var cognomi =["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
+console.log("array cognomi: ", cognomi);
+
+// cognome utente da inserire
+var cognomeUtente = prompt("inserisci il cognome con la lettera maiuscola");
+console.log("cognome utente: ",cognomeUtente);
+// cognome utente inserito nell'array
+cognomi.push(cognomeUtente);
+
+// array cognomi dopo l'inserimento del cognome utente
+console.log("array cognomi modificato: ", cognomi);
+
+// ordina i cognomi in ordine alfabetico
+var cognomiOrdinati = cognomi.sort();
+
+// stampa cognomi in ordine alfabetico
+console.log("array cognomi: ", cognomiOrdinati);
+
+// stampa cognomi con posizione
+for(i=0; i < cognomi.length; i++) {
+    console.log("Cognome: ", cognomi[i] + ", " + "In posizione:", i + 1);
+}
