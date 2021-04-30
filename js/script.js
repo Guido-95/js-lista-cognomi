ea98f8473164b64d846204903db8088c3066af64
Guido-95 <guidocarna1995@gmail.com>
2021-04-30 16:59:15 +0200

    sistemazione codice

--- js/script.js
+++ js/script.js
@@ -4,27 +4,28 @@
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 
 // array cognomi iniziale
-var cognomi =["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 console.log("array cognomi: ", cognomi);
 
 // cognome utente da inserire
 var cognomeUtente = prompt("inserisci il cognome con la lettera maiuscola");
 
-// controllo lettera maiuscola
-while(cognomeUtente[0] != cognomeUtente[0].toUpperCase()){
+// controllo che la prima lettera sia maiuscola
+while(cognomeUtente[0] != cognomeUtente[0].toUpperCase()) {
     cognomeUtente = prompt("ERRORE, inserisci il cognome con la lettera maiuscola");
 }
-// cognome inserito dall'utente
-console.log("cognome utente: ",cognomeUtente);
+// stampa cognome inserito dall'utente
+console.log("cognome utente: ", cognomeUtente);
 
 // cognome utente inserito nell'array
 cognomi.push(cognomeUtente);
 
-// array cognomi dopo l'inserimento del cognome utente
+// stampa array cognomi con il cognome utente aggiunto
 console.log("array cognomi modificato: ", cognomi);
 
 // ordina i cognomi in ordine alfabetico
 var cognomiOrdinati = cognomi.sort();
+
 // stampa cognomi in ordine alfabetico
 console.log("array cognomi ordinato: ", cognomiOrdinati);
 
